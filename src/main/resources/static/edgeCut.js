b426cfa0ee40a7512dfde3a6e97eb7ec585ffe6e
huanyu.why <why490078184@gmail.com>
2017-10-26 23:15:57 +0800

    优化视觉&增加人工修改和未修改的分类

--- src/main/resources/static/edgeCut.js
+++ src/main/resources/static/edgeCut.js
@@ -1,17 +1,18 @@
 (($)=>{
 
     let list = $('#list');
+    let fixedList = $('#fixedList');
     let getId = (key)=>{
         return '_' + key.replace('.','').replace('/','');
     };
     let search = new URL(window.location.href).searchParams;
     let all = {};
     let listItemTemplate = (imgUrl,key)=>{
-        return `    <div class="col-xs-6">
-    <div >
+        return `    <div class="col-xs-4">
+    <div class="my-thumbnail">
       <img id="${getId(key)}" src="${imgUrl}">
       <div class="caption">
-        <p><a data-originkey="${key}" data-key="${getId(key)}" class="alter-pic btn btn-primary" role="button">确认修改</a></p>
+        <a data-originkey="${key}" data-key="${getId(key)}" class="alter-pic btn btn-primary btn-block" role="button">确认修改</a>
       </div>
     </div>
 </div>
@@ -19,47 +20,58 @@
     };
 
     let fetchPic = ()=>{
-        $.ajax(
-            {
-                url:'//101.132.181.91/service/result',
-                data:{
-                    prefix:search.get('prefix') || "1",
-                    count:search.get('count') || "100"
-                },
-                dataType:'jsonp',
-                success:(res)=>{
-                    // console.log (res.data);
-                    let result = res.data.map((item)=>{
-                        return listItemTemplate(item.originDownloadUrl+'?x-oss-process=image/format,jpg/quality,q_20',item.key);
-                    }).join('');
-                    // console.log (result);
-                    // list.append(result)
-                    list.html(result);
-                    res.data.forEach((item)=>{
-                        $(`#${getId(item.key)}`).Jcrop({
-                            boxWidth:600
-                        },function () {
-                            // this.setSelect([100,100,200,150],function (a) {
-                            //     console.log ('123132');
-                            // })
+        return new Promise((resolve)=>{
+            $.ajax(
+                {
+                    url:'//101.132.181.91/service/result',
+                    data:{
+                        prefix:search.get('prefix') || "1",
+                        count:search.get('count') || "10"
+                    },
+                    dataType:'jsonp',
+                    success:(res)=>{
+                        // console.log (res.data);
+                        let result = res.data.filter((item)=>{
+                            return item.status === '0';
+                        }).map((item)=>{
+                            return listItemTemplate(item.originDownloadUrl+'?x-oss-process=image/format,jpg/quality,q_20',item.key);
+                        }).join('');
+                        let fixedResult = res.data.filter((item)=>{
+                            return item.status === '1';
+                        }).map((item)=>{
+                            return listItemTemplate(item.originDownloadUrl+'?x-oss-process=image/format,jpg/quality,q_20',item.key);
+                        })
+                        // console.log (result);
+                        // list.append(result)
+                        list.html(result);
+                        fixedList.html(fixedResult);
+                        res.data.forEach((item)=>{
+                            $(`#${getId(item.key)}`).Jcrop({
+                                boxWidth:window.screen.width/3 -20
+                            },function () {
+                                // this.setSelect([100,100,200,150],function (a) {
+                                //     console.log ('123132');
+                                // })
 
-                            this.setSelect([item.x,item.y,item.x+item.w,item.y+item.h]);
-                            all[getId(item.key)] = this;
+                                this.setSelect([item.x,item.y,item.x+item.w,item.y+item.h]);
+                                all[getId(item.key)] = this;
+                            });
+
                         });
-
-                    })
+                        resolve(res);
+                    }
                 }
-            }
-        )
+            )
+        })
     };
 
-    let init = ()=>{
-        fetchPic();
+
+    fetchPic().then((res)=>{
+
         $('body').on('click','.alter-pic',(e)=>{
             let key = e.target.dataset.key;
             let originKey = e.target.dataset.originkey;
             let current = all[key].tellSelect ();
-            console.log (current);
             $.ajax({
                 url:'http://101.132.181.91/service/update',
                 data:{
@@ -68,16 +80,13 @@
                     y:Math.round(current.y),
                     w:Math.round(current.w),
                     h:Math.round(current.h)
-
                 },
                 dataType:'jsonp',
                 success:(res)=>{
-                    console.log (res);
                     $.jGrowl('修改成功');
+                    fetchPic();
                 }
             })
         });
-    };
-
-    init();
+    });
 })(jQuery);
